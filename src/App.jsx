555e940916284e0f1dcb2d57cc636f9c43bc1562
Max Roberts <133835313+TheMaxRob@users.noreply.github.com>
2024-03-04 12:41:17 +0000

    Created custom buton, input, and NoProjectSelected

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import ProjectsSidebar from "./ProjectsSidebar";
+import { useState } from "react";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from './components/NewProject';
+import NoProjectSelected from "./components/NoProjectSelected";
+
 
 function App() {
+
+  const [ projectsState, setProjectsState ] = useState({
+    selectedProjectID: undefined,
+    projects: []
+  });
+
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NoProjectSelected />
     </main>
   );
 }
